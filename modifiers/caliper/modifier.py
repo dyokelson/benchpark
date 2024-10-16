e977fcd96accfde8c0d653d8839482647ba5fd03
dyokelson <dewi.yokelson@gmail.com>
2024-10-16 15:18:38 -0700

    update to caliper master for metadata

--- modifiers/caliper/modifier.py
+++ modifiers/caliper/modifier.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from ramble.modkit import *
+import json
 
 
 class Caliper(SpackModifier):
@@ -19,14 +20,21 @@ class Caliper(SpackModifier):
 
     _cali_datafile = "{experiment_run_dir}/{experiment_name}.cali"
 
+    # Write experiment metadata to a json file for caliper to add to metadata
     _experiment_metadata_file = "{experiment_run_dir}/{experiment_name}_metadata.json"
-
-    application_name = {name}
+    application_name = "{exe_name}"
+    experiment_name = "{experiment_name}"
+    metadata_dict = {"application_name": application_name, "experiment_name": experiment_name}
+    # Serializing json
+    json_object = json.dumps(metadata_dict, indent=4)
+    # Writing to sample.json
+    with open(_experiment_metadata_file, "w") as outfile:
+        outfile.write(json_object)
 
     env_var_modification(
         "CALI_CONFIG",
-        #"spot(output={}),metadata(file={})".format(_cali_datafile, _experiment_metadata_file),
-        "spot(output={}),metadata(application={})".format(_cali_datafile, application_name),
+        "spot(output={}),metadata(file={})".format(_cali_datafile, _experiment_metadata_file),
+        #"spot(output={}),metadata(application={})".format(_cali_datafile, application_name),
         method="set",
         modes=["time"],
     )
